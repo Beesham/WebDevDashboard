3d8918dd0262e4a9be682f5e6045ae66b5424961
Beesham Sarendranauth <sarendr@cs.uwindsor.ca>
2019-03-21 15:27:31 -0400

    Refactor: use avoid html attack in register_user.php

--- public_html/php/register_user.php
+++ public_html/php/register_user.php
@@ -10,15 +10,19 @@ if(array_key_exists('firstname', $_POST) &&
    array_key_exists('password', $_POST) &&
    count($_POST) == 4 ) {
     
-    //TODO: check if user already exists. register user
-
-    if(UserManager::checkIfUserExist($_POST['email'])) {
+    if(UserManager::checkIfUserExist(htmlspecialchars($_POST['email']), ENT_QUOTES)) {
         //TODO: user exists send error
+        $_SESSION = "";
     } else {
-        $status = UserManager::addUser($_POST['firstname'],$_POST['lastname'],$_POST['email'],$_POST['password']);
+        $status = UserManager::addUser(
+                            htmlspecialchars($_POST['firstname'], ENT_QUOTES),
+                            htmlspecialchars($_POST['lastname'], ENT_QUOTES),
+                            htmlspecialchars($_POST['email'], ENT_QUOTES),
+                            htmlspecialchars($_POST['password'], ENT_QUOTES));
         echo $status;
         if($status) {
-            login_user_registered($_POST['email'], $_POST['password']);
+            login_user_registered(htmlspecialchars($_POST['email'], ENT_QUOTES),
+                                    htmlspecialchars($_POST['password'], ENT_QUOTES));
         }
     }
 }
